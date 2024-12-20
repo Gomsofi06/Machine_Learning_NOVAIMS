72d43b250b5e907ee1d53332b5e87762be30e84d
Rui-Lourenco <rui.lourenco.creator@gmail.com>
2024-12-20 20:25:27 +0000

    new pb

--- 05_3 GridSearch XGBoost.py
+++ 05_3 GridSearch XGBoost.py
@@ -81,7 +81,7 @@ y_val_ray = ray.put(y_val)
 search_space = {
     # Model Dependent
     "n_estimators": tune.grid_search([200, 300]),         
-    "learning_rate": tune.grid_search([0.1]),   # [0.01, 0.03, 0.05, 0.1, 1.2]  
+    "learning_rate": tune.grid_search([0.2]),   # [0.01, 0.03, 0.05, 0.1, 0.2]  
     "max_depth": tune.grid_search([6, 7]),                              
     "subsample": tune.grid_search([0.8, 0.9]),            
     "colsample_bytree": tune.grid_search([0.8, 0.9]),
